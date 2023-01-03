345cf8e2300f3022576526c1ac866d2d8d88c39d
lizolson <45490630+lizolson@users.noreply.github.com>
2023-01-03 11:45:43 -0500

    Update Laser_Scan_Handler.cpp

--- ROS/catkin_ws/src/fetch_teleop_depth/src/Laser_Scan_Handler.cpp
+++ ROS/catkin_ws/src/fetch_teleop_depth/src/Laser_Scan_Handler.cpp
@@ -5,12 +5,12 @@ Laser_Scan_Handler::Laser_Scan_Handler(ros::NodeHandle &nh)
     nh_ = nh;
     subscriber_ = nh_.subscribe("/base_scan", 1, &Laser_Scan_Handler::Laser_Scan_Callback, this);
 
-    //TODO: initialize a publisher that is set to the channel "/minimum_scan_distance"
+    //TODO: initialize a publisher that is set to the channel "minimum_scan_distance"
 }
 
 void Laser_Scan_Handler::Laser_Scan_Callback(const sensor_msgs::LaserScan::ConstPtr &msg_laser_scan)
 {
-    //TODO: Given the incoming laser scan message, find the minimium distance scan, and publish this to "/minimum_scan_distance"
+    //TODO: Given the incoming laser scan message, find the minimium distance scan, and publish this to "minimum_scan_distance"
 
 
 }
