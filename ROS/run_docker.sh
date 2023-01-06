1736c72d2c114f188df8944cd29dcc1e10561193
lizolson <45490630+lizolson@users.noreply.github.com>
2023-01-06 15:08:54 -0500

    Update run_docker.sh

--- ROS/run_docker.sh
+++ ROS/run_docker.sh
@@ -1,1 +1,1 @@
-docker run -p 6080:80 --rm --shm-size=512m  -v $(pwd):/home/ubuntu/Desktop/ fetch_demo:latest
+docker run -p 6080:80 --rm --shm-size=512m  -v "$(pwd):/home/ubuntu/Desktop/" fetch_demo:latest
