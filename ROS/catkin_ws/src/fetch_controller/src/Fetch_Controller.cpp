cc96c97bfcaa312d9220490b2070e2ced2c237a9
lizolson <lizolson@umich.edu>
2023-01-08 21:59:38 -0500

    clearer instructions

--- ROS/catkin_ws/src/fetch_controller/src/Fetch_Controller.cpp
+++ ROS/catkin_ws/src/fetch_controller/src/Fetch_Controller.cpp
@@ -4,7 +4,7 @@ Fetch_Controller::Fetch_Controller(ros::NodeHandle &nh)
 {
     nh_ = nh;
 
-    //TODO: initialize a subscriber that is set to the channel "/base_scan"
+    //TODO: initialize a subscriber that is set to the channel "/base_scan". Set its callback function to be Laser_Scan_Callback
     //TODO: initialize a publisher that is set to the channel "/cmd_vel"
 
 
@@ -14,7 +14,8 @@ void Fetch_Controller::Laser_Scan_Callback(const sensor_msgs::LaserScan::ConstPt
 {
     /*TODO: 
     Given the incoming laser scan message, find the minimium distance of the front facing scans
-    Hint: Consider scans 241 - 421 instead of 0-661
+    Hint: The laser scan measuring directly in front of the robot will be the scan at the middle of the array laser scans. 
+    So for finding the minimum, we will ONLY consider the 100 laser scans in the middle of the array of laser scans. 
     If the minimum scan in this direction is greater than 1m, drive forward. 
     Otherwise, turn left. 
     */
