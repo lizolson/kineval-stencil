548f7c6bb8f7dfb6008e410688c428036e923a44
lizolson <45490630+lizolson@users.noreply.github.com>
2023-01-08 23:21:05 -0500

    Update Fetch_Controller.cpp

--- ROS/catkin_ws/src/fetch_controller/src/Fetch_Controller.cpp
+++ ROS/catkin_ws/src/fetch_controller/src/Fetch_Controller.cpp
@@ -15,7 +15,7 @@ void Fetch_Controller::Laser_Scan_Callback(const sensor_msgs::LaserScan::ConstPt
     /*TODO: 
     Given the incoming laser scan message, find the minimium distance of the front facing scans
     Hint: The laser scan measuring directly in front of the robot will be the scan at the middle of the array laser scans. 
-    So for finding the minimum, we will ONLY consider the 200 laser scans in the middle of the array of laser scans. 
+    So for finding the minimum, we will ONLY consider the 120 laser scans in the middle of the array of laser scans. 
     If the minimum scan in this direction is greater than 1m, drive forward. 
     Otherwise, turn left. 
     */
