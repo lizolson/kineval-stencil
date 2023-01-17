6066aa496fd7fb0972bedc2f47448bdfbbe89ea3
Liz Olson <lizolson@umich.edu>
2023-01-17 02:41:27 -0500

    Update grades at time: 17-01-2023-02-32-34

---
+++ grade.js
@@ -0,0 +1,264 @@
+//CI grader generates report at 17-01-2023-02-08-28
+grade = 
+{
+  "lizolson": {
+    "name": "Olson, Liz",
+    "unique_name": "lizolson",
+    "email": "lizolson@umich.edu",
+    "course": "ROB 511 W23",
+    "repository": "lizolson/kineval-stencil",
+    "assignments": {
+      "Honor": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "ROS Pub/Sub": {
+        "status": "PENDING",
+        "comments": [
+          "Passing 0 of 3 tests"
+        ]
+      },
+      "PathPlan_Heap": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "PathPlan_AStar": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "PathPlan_DFS": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "PathPlan_BFS": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "PathPlan_BestFirst": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pendularm_Euler": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pendularm_VelocityVerlet": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pendularm_PID": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pendularm_Verlet": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pendularm_RK4": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pendularm_Double": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FK_MatrixRoutines": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FK_Transforms": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FK_JointRendering": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FK_BaseOffset": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FK_NewRobot": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_Quaternion": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_BaseControl": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_SetpointControl": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_FSM": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_JointLimits": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_Prismatic": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_Rosbridge": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "IK_Jacobian": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "IK_JTranspose": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "IK_JPseudoInverse": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "IK_EulerAngles": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "MotionPlan_Collision": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "MotionPlan_2DRRTConnect": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "MotionPlan_CSpaceRRTConnect": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "MotionPlan_RRTStar": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pitch": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pitch_Advanced": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Advanced": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      }
+    },
+    "quizzes": [
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      }
+    ]
+  }
+}
